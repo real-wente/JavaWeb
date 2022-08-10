6b8e2c632c8ab115f7f2a8b5a80cb0ac6f790354
wentao <614935302@qq.com>
2022-08-10 17:14:10 +0800

    1

---
+++ src/main/java/Day1/junit/Calculator.java
@@ -0,0 +1,34 @@
+package Day1.junit;
+
+/**
+ * 计算器类
+ * @author wentao
+ * @date 2022-08-10  17:06
+ */
+
+public class Calculator {
+
+    /**
+     * 加法
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int add (int num1,int num2){
+        return num2+num1;
+    }
+
+    /**
+     * 减法
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public int sub (int num1,int num2){
+        return num1-num2;
+    }
+
+
+
+
+}
---
+++ src/main/java/Day1/junit/CalculatorTest.java
@@ -0,0 +1,10 @@
+package Day1.junit;
+
+/**
+ * @author wentao
+ * @date 2022-08-10  17:12
+ */
+
+public class CalculatorTest {
+
+}
